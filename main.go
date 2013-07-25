1c0c886e7ba76528d72a537fc9b8dd61ba1c60c1
Sean Kell <sean.michael.kell@gmail.com>
2013-07-24 22:25:35 -0400

    Add index.html.tmpl, and handler to serve it.

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
-	fmt.Println("listening...")
+	http.HandleFunc("/", index)
+	log.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello, heroku")
+func index(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Responding to request %s with index handler.\n", r.URL)
+	t, err := template.ParseFiles("./client/index.html.tmpl")
+	if err != nil {
+		log.Println(err)
+	}
+
+	t.Execute(w, nil)
 }
