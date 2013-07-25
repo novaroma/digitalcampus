d2cb3c3090d78b83f479f2ef9c6d14dc42500321
Sean Kell <sean.michael.kell@gmail.com>
2013-07-25 13:26:09 -0400

    Add platform specific line endings.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
-// Copyright (c) 2013 Nova Roma. All rights resrved. 
-// This Source Code Form is subject to the terms of the Mozilla Public License, 
-// v.2.0. If a copy of the MPL was not distributed with this file, You can 
-// obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright (c) 2013 Nova Roma. All rights reserved. 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 package main
 
 import (
@@ -12,9 +12,12 @@ import (
 )
 
 func main() {
+	// Handler function registrations
 	http.HandleFunc("/", index)
 	http.Handle("/content/", http.StripPrefix("/content/", http.FileServer(http.Dir("./client"))))
-	log.Println("listening...")
+
+	// Start up the server 
+	log.Printf("listening on port %s...%s", os.Getenv("PORT"), lineEnding)
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
@@ -22,11 +25,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Responding to request %s with index handler.\n", r.URL)
+	log.Printf("Responding to request %s with index handler.%v", r.URL, lineEnding)
 	t, err := template.ParseFiles("./client/index.html.tmpl")
 	if err != nil {
 		log.Println(err)
 	}
 
 	t.Execute(w, nil)
+	log.Printf("Successfully handled request %s.", r.URL)
 }
